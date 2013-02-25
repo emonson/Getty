ca72d2f5516cd47713d70ae2c80183bdd1e7713e
Eric E Monson <emonson@cs.duke.edu>
2013-02-25 15:22:46 -0500

    trying different price parsing options with nltk

--- price_parsing.py
+++ price_parsing.py
@@ -1,5 +1,6 @@
 import nltk
 import os
+import re
 import codecs
 
 data_dir = '/Users/emonson/Data/Getty'
@@ -10,21 +11,25 @@ data_in = codecs.open(data_path, 'r', 'utf-8')
 
 # Tokenizer
 token_patterns = r'''(?x)    # set flag to allow verbose regexps
-         ([A-Z]\.)+        # abbreviations, e.g. U.S.A. 
-       | \w+(-\w+)*        # words with optional internal hyphens
-       | \$?\d+\.?\d+?%?  # currency and percentages, e.g. $12.40, 82%
-       | \.\.\.            # ellipsis
-       | [][.,;"'?():-_`]  # these are separate tokens
+         \|\w        			# subsection dividers 
+       | \[\w\]        		# lot tags
+       | |\d+\.?(\d+)?  	# numbers (currency amounts)
+       | |\w+            	# other words
     '''
+
 # token_patterns = r'\|\w|\[[a-zA-Z- ]+\]|\w+|\d+(?:[.\d]+)?|\W+|\S+'
-# tokenizer = nltk.tokenize.RegexpTokenizer(token_patterns)
-tokenizer = nltk.tokenize.RegexpTokenizer(r'\|\w|\[\w\]|\d+\.?(\d+)?|\w+')
+tokenizer = nltk.tokenize.RegexpTokenizer(token_patterns)
+tokenizer = nltk.tokenize.RegexpTokenizer(r'\|\w|\[\w\]|\[\w-\w\]|\d+\.?(\d+)?|[&]|\w+')
+
 
 # Define tagger
-chunk_patterns = [(r'[0-9]+', 'NUM'),
+chunk_patterns = [(r'[0-9]+(?:\.[0-9]+)?', 'NUM'),
 						(r'livre(s)?', 'CUR'),
 						(r'franc(s)?', 'CUR'),
+						(r'^ou$', 'OR'),
+						(r'&', 'AND'),
 						(r'\[\w\]', 'LOTMOD'),
+						(r'\[\w-\w\]', 'LOTRNG'),
 						(r'\[\?\]', 'QU'),
 						(r'\|\w', 'NOTE'),
 						(r'.*', 'O')
@@ -32,21 +37,38 @@ chunk_patterns = [(r'[0-9]+', 'NUM'),
 
 regexp_tagger = nltk.RegexpTagger(chunk_patterns)
 
+re_subfield = re.compile(r'(\|\w) *')
+
 for line in data_in:
 	cols = line.split(',')
 	price = cols[2].strip(' "\n\r')
 	
-	if len(price) > 15:
-		# Tagging
-		# tokens = nltk.word_tokenize(price)
-		tokens = tokenizer.tokenize(price)
-		# tokens = nltk.regexp_tokenize(price, token_patterns)
-		print price
-		for x in tokens:
-			print x + ' ',
-		print
-		# print
-		tagged = regexp_tagger.tag(tokens)
-# 		print price
-		print tagged
-		print
+	if price:
+		
+		# split on |c type subsection divider
+		price_subfields = re_subfield.split(price)
+		subfield_flag = None
+		
+		for price_field in price_subfields:
+			if price_field == '|c':
+				subfield_flag = 'currency'
+				continue
+			elif price_field == '|d':
+				subfield_flag = 'notes'
+				continue
+			else:
+				subfield_flag = None
+			
+			# Tagging
+			# tokens = nltk.word_tokenize(price)
+			tokens = tokenizer.tokenize(price_field)
+			# tokens = nltk.regexp_tokenize(price, token_patterns)
+			print price_field
+			for x in tokens:
+				print x + ' ',
+			print
+			# print
+			tagged = regexp_tagger.tag(tokens)
+	# 		print price
+			print tagged
+			print
