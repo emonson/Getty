6f8b0a6d90e1098c613bc0dbda18814e8dfef380
Eric E Monson <emonson@cs.duke.edu>
2014-11-16 15:24:34 -0500

    improving price parsing

--- price_parsing.py
+++ price_parsing.py
@@ -24,14 +24,14 @@ db = db_conn['getty']
 
 # Tokenizer
 # token_patterns = r'''
-#          \|\w        			# subsection dividers 
-#        | \[\w\]        		# lot tags
-#        | \[\w-\w\]				# lot tag range
-#        | \d+\.?(\d+)?  		# numbers (currency amounts)
-#        | [&]							# ampersand
-#        | \[\?\]						# question mark
-#        | lots							# lots indicator
-#        | \w+            	# other words
+#          \|\w                 # subsection dividers 
+#        | \[\w\]               # lot tags
+#        | \[\w-\w\]                # lot tag range
+#        | \d+\.?(\d+)?         # numbers (currency amounts)
+#        | [&]                          # ampersand
+#        | \[\?\]                       # question mark
+#        | lots                         # lots indicator
+#        | \w+              # other words
 #     '''
 # 
 # # token_patterns = r'\|\w|\[[a-zA-Z- ]+\]|\w+|\d+(?:[.\d]+)?|\W+|\S+'
@@ -41,22 +41,23 @@ db = db_conn['getty']
 
 # Define tagger
 tag_patterns = [(r'[0-9]+(?:(?:\.|:)[0-9]+){0,2}', 'NUM'),
-						(r'livre(s)?', 'CUR'),
-						(r'franc(s)?', 'CUR'),
-						(r'fl', 'CUR'),
-						(ur'\u00A3', 'CUR'),
-						(r'^ou$', 'OR'),
-						(r'&', 'AND'),
-						(r'and', 'AND'),
-						(ur'\u00E0', 'RNG'),
-						(r'lots', 'LOTS'),
-						(r'\[\w\]', 'LOTMOD'),
-						(r'\[\w-\w\]', 'LOTRNG'),
-						(r'\[\?\]', 'QU'),
-						(r'\|\w', 'SUBFIELD'),
-						(r'-', 'RNG'),
-						(r'\w+', 'O')
-						]
+                        (r'livre(s)?', 'CUR'),
+                        (r'franc(s)?', 'CUR'),
+                        (r'fl', 'CUR'),
+                        (ur'\u00A3', 'CUR'),
+                        (r'^ou$', 'OR'),
+                        (r'&', 'AND'),
+                        (r'and', 'AND'),
+                        (ur'\u00E0', 'RNG'),
+                        (r'lots', 'LOTS'),
+                        (r'nos\.?', 'NOS'),
+                        (r'\[[a-z][a-z]?\]', 'LOTMOD'),
+                        (r'\[[a-z][a-z]?-[a-z][a-z]?\]', 'LOTRNG'),
+                        (r'\[\?\]', 'QU'),
+                        (r'\|[a-z]', 'SUBFIELD'),
+                        (r'-', 'RNG'),
+                        (r'\w+', 'O')
+                        ]
 
 # Defining tokenizing patterns directly from tag patterns so only defined once
 token_patterns = '|'.join([x for x,y in tag_patterns]).strip('|')
@@ -66,17 +67,19 @@ regexp_tagger = nltk.RegexpTagger(tag_patterns)
 
 
 # Chunker
+# TODO: Still need OR variants...
 price_grammar = '''
-	AMT: 	{<NUM><CUR><NUM>}
-				{<NUM><CUR>}
+    AMT:    {<NUM><CUR><NUM>}
+                {<NUM><CUR>}
 '''
 ch_amt = nltk.RegexpParser(price_grammar)
 
 # Note: the chunker seems to be NOT greedy, so it'll quit as soon as it
 #   finds a match (and won't continue as long as it can)...
+# TODO: still need num, num & num variant...
 lots_grammar = '''
-	INC:	{<LOTS><O>*<NUM><RNG><NUM>}
-				{<LOTS><O>*(<AND>?<NUM>(<LOTMOD>|<LOGRNG>)?)+}
+    INC:    {<LOTS><NOS>(<NUM><RNG><NUM>)}
+                {<LOTS><NOS>(<AND>?<NUM>(<LOTMOD>|<LOTRNG>)?)+}
 '''
 ch_lots = nltk.RegexpParser(lots_grammar)
 
@@ -84,60 +87,59 @@ re_subfield = re.compile(r'(\|\w) *')
 
 # File method
 # for line in data_in:
-# 	cols = line.split(',')
-# 	price = cols[2].strip(' "\n\r')
+#   cols = line.split(',')
+#   price = cols[2].strip(' "\n\r')
 # Direct from MongoDB method
-for entry in db.sales.find({'price':{'$exists':True},'data_file':{'$regex':r'rand'}},{'price':True}):
-	price = entry['price']
-	
-	if price:
-		
-		# split on |c type subsection divider
-		price_subfields = re_subfield.split(price)
-		subfield_flag = None
-		
-		for price_field in price_subfields:
-			if price_field == '|c':
-				subfield_flag = 'currency'
-				continue
-			elif price_field == '|d':
-				subfield_flag = 'notes'
-				continue
-			
-			if price_field:
-				# Tagging
-				# tokens = nltk.word_tokenize(price)
-				tokens = tokenizer.tokenize(price_field)
-				# tokens = nltk.regexp_tokenize(price, token_patterns)
-				print price_field
-				for x in tokens:
-					print x + ' ',
-				print
-				# print
-				tagged = regexp_tagger.tag(tokens)
-		# 		print price
-				print tagged
-			
-				# Chunking
-				if subfield_flag == None:
-					tree = ch_amt.parse(tagged)
-					print tree
-					for subtree in tree.subtrees():
-						if subtree.node == 'AMT':
-							print 'AMT', subtree.leaves()
-			
-				elif subfield_flag == 'notes':
-					tree = ch_lots.parse(tagged)
-					n_incs = 0
-					for subtree in tree.subtrees():
-						if subtree.node == 'INC':
-							# Note: problem printing subtree if one of the entries is a unicode non-ascii...
-							print 'INC', subtree.leaves()
-							n_incs += 1
-					if n_incs == 0:
-						print '*** NOTES NOT CAUGHT **', len(price)
-			
-				
-			print
-			# Only hits this after processing a line, not a tag
-			subfield_flag = None
+for entry in db.sales.find({'price':{'$exists':True},'country_authority':'France'},{'price':True}):
+    price = entry['price']
+    
+    if price.find(',') >= 0:
+        print price
+        
+        # split on |c type subsection divider
+        price_subfields = re_subfield.split(price)
+        subfield_flag = None
+        
+        for price_field in price_subfields:
+            
+            # Sometimes there are just empty currency or notes fields
+            if price_field == '|c':
+                subfield_flag = 'currency'
+                continue
+            elif price_field == '|d':
+                subfield_flag = 'notes'
+                continue
+            
+            if price_field:
+                # Tagging
+                # tokens = nltk.word_tokenize(price)
+                tokens = tokenizer.tokenize(price_field)
+                # tokens = nltk.regexp_tokenize(price, token_patterns)
+                print '    ', '_'.join(tokens)
+
+                tagged = regexp_tagger.tag(tokens)
+                print tagged
+            
+                # Chunking
+                if subfield_flag == None:
+                    tree = ch_amt.parse(tagged)
+                    print tree
+                    for subtree in tree.subtrees():
+                        if subtree.label() == 'AMT':
+                            print 'AMT', subtree.leaves()
+            
+                elif subfield_flag == 'notes':
+                    tree = ch_lots.parse(tagged)
+                    n_incs = 0
+                    for subtree in tree.subtrees():
+                        if subtree.label() == 'INC':
+                            # Note: problem printing subtree if one of the entries is a unicode non-ascii...
+                            print 'INC', subtree.leaves()
+                            n_incs += 1
+                    if n_incs == 0:
+                        print '*** NOTES NOT CAUGHT **', len(price)
+            
+                
+            print
+            # Only hits this after processing a line, not a tag
+            subfield_flag = None
