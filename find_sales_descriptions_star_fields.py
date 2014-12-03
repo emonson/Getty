449e4b86c09e5b6001b5012353b39db3c8a0b212
Eric E Monson <emonson@cs.duke.edu>
2014-12-03 15:19:27 -0500

    added script which finds fields and repeated fields in new sales descriptions data, and think I got the descriptions data into the mongodb database (not parsed at all -- dates, etc)

---
+++ find_sales_descriptions_star_fields.py
@@ -0,0 +1,66 @@
+import os
+import codecs
+import collections
+
+data_dir = '/Users/emonson/Data/Getty/18th_century'
+script_dir = '/Users/emonson/Programming/ArtMarkets/Getty'
+
+# Sales descriptions data (about the auctions themselves, not the "contents", which are the lots up for sale)
+descriptions_file = '18th_cent_sales_descriptions.txt'
+descriptions_path = os.path.join(data_dir, descriptions_file)
+
+# Fast file line count subroutine
+# http://stackoverflow.com/questions/845058/how-to-get-line-count-cheaply-in-python
+import subprocess
+def file_len(fname):
+	p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	result, err = p.communicate()
+	if p.returncode != 0:
+		raise IOError(err)
+	return int(result.strip().split()[0])
+	
+print "Loading Sales Contents into DB from", descriptions_file
+n_lines = file_len(descriptions_path)
+data_in = codecs.open(descriptions_path, 'r', 'iso-8859-1')
+
+fields = collections.Counter()
+repeat_fields = set()
+record_fields_list = []
+
+for ii, line in enumerate(data_in):
+    if ii % 10000 == 0:
+        print str(ii).rjust(len(str(n_lines))), '/', n_lines
+    
+    # continued lines
+    if line.startswith(' '):
+        continue
+
+    star_field_name = line[:17].strip()
+    value = line[17:].strip()
+
+    # blank lines
+    if not star_field_name:
+        continue
+
+    if star_field_name == '--RECORD NUMBER--':
+        fields.update(record_fields_list)
+        record_fields_set = set(record_fields_list)
+        if len(record_fields_set) != len(record_fields_list):
+            record_fields_count = collections.Counter(record_fields_list)
+            for field_name,count in record_fields_count.iteritems():
+                if count > 1:
+                    repeat_fields.add(field_name)
+        record_fields_list = []
+    else:
+        record_fields_list.append(star_field_name)
+
+print 'All fields'
+print fields.most_common(100)
+
+print 'Repeated fields'
+print repeat_fields
+
+for k in fields:
+    print k
+
+
--- sales_descriptions_parsing.py
+++ sales_descriptions_parsing.py
@@ -3,6 +3,7 @@ import re
 import sys
 import codecs
 from datetime import datetime
+import pandas as pd
 from pymongo import Connection
 
 data_dir = '/Users/emonson/Data/Getty/18th_century'
@@ -13,7 +14,7 @@ descriptions_file = '18th_cent_sales_descriptions.txt'
 descriptions_path = os.path.join(data_dir, descriptions_file)
 
 # Tab-separated values text file describing data fields, their types, and whether they repeat
-fields_file = '18th_cent_format_and_fields.txt'
+fields_file = '18th_cent_descriptions_fields.xlsx'
 fields_path = os.path.join(script_dir, fields_file)
 
 re_dot = re.compile(r'\.')
@@ -25,12 +26,12 @@ re_spmult = re.compile(r' {2,}')
 # http://stackoverflow.com/questions/845058/how-to-get-line-count-cheaply-in-python
 import subprocess
 def file_len(fname):
-	p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	result, err = p.communicate()
-	if p.returncode != 0:
-		raise IOError(err)
-	return int(result.strip().split()[0])
-	
+    p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result, err = p.communicate()
+    if p.returncode != 0:
+        raise IOError(err)
+    return int(result.strip().split()[0])
+    
 # Make a connection to Mongo.
 try:
     db_conn = Connection()
@@ -43,156 +44,138 @@ db = db_conn['getty_18th']
 
 # * * DANGER !!!! * *
 db.descriptions.drop()
-db.fields_descriptions.drop()
+db.descriptions_fields.drop()
 # * *
 
 # Descriptions fields
 
-# Creating dictionaries so when we encounter a field name in the sales descriptions
+# Creating dictionaries so when we encounter a field name in the STAR sales descriptions
 # text dump file, we'll know other things about it for use later in parsing the dump file:
 #    whether it repeats
 #    if it repeats, whether it's in a "block" of repeating fields
 #    if it's non-string, what the data type is (how it should be interpreted/parsed)
 #    
 print "Loading Fields into DB"
-fields_in = codecs.open(fields_path, 'r', 'utf-8')
-sales_fields = {}
-sales_blocks = {}
-sales_repeats = {}
-sales_formats = {}
+df = pd.read_excel(open(fields_path,'r'))
 
-# Read in titles line
-titles = fields_in.readline().split('\t')
-field_field_names = [re_spdash.sub('_',re_dot.sub('',x.strip().lower())) for x in titles]
+# This will be keyed by the STAR_field_name
+fields_properties = {}
 
-for line in fields_in:
-	data = line.rstrip().split('\t')
-	
-	# Allow comment lines in fields file
-	if data[0].startswith('#'):
-		continue
-		
-	field_doc = {}
-	for ii, item in enumerate(data):
-		# strip double quotes and turn multiple spaces into single
-		item_base = re_spmult.sub(' ', item.strip(' "'))
-		# If anything left after stripping extras
-		if item_base:
-			field_doc[field_field_names[ii]] = item_base
-			# Reduce full field name to lowercase underscore version
-			if field_field_names[ii] == 'full_field_name':	
-				item_base_under = re_spdash.sub('_',re_dotcomma.sub('',item_base.strip().lower()))
-				field_doc['db_field_name'] = item_base_under
-				# Record dictionary of file_label -> db_field_name
-				sales_fields[field_doc['file_label']] = item_base_under
-			if field_field_names[ii] == 'repeat':
-				sales_repeats[field_doc['file_label']] = item_base
-			if field_field_names[ii] == 'block':
-				sales_blocks[field_doc['file_label']] = item_base
-			if field_field_names[ii] == 'subfields':
-				sales_formats[field_doc['file_label']] = item_base
-				
-	db.fields.save(field_doc)
+for d in df.iterrows():
+    # DataFrame.iterrows() returns a tuple with (index, Series), so have to pull apart d[1] to get row items
+    # Get rid of NaNs from each row before saving doc to database and creating dict for use later
+    # That way can test whether 'can_repeat' is in dict
+    field_doc = dict([(k.lower(),v) for (k,v) in d[1].to_dict().iteritems() if pd.notnull(v)])
+    fields_properties[field_doc['star_field_name']] = field_doc
+    db.descriptions_fields.save(field_doc)
 
 
-# Sales descriptions actual data
-for data_file in data_files:
+print "Loading Sales Contents into DB from", descriptions_file
+n_lines = file_len(descriptions_path)
+data_in = codecs.open(descriptions_path, 'r', 'iso-8859-1')
 
-	print "Loading Sales Contents into DB from", data_file
-	data_path = os.path.join(data_dir, data_file)
-	n_lines = file_len(data_path)
-	data_in = codecs.open(data_path, 'r', 'iso-8859-1')
+doc = {}
+current_field = None
+current_block = None
+index = 0
+db_field = None
+block = None
+star_field_name = None
+value = None
 
-	doc = {}
-	current_field = None
-	current_block = None
-	index = 0
-	field = None
-	block = None
-	key = None
-	value = None
+for ii, line in enumerate(data_in):
+    if ii % 10000 == 0:
+        print str(ii).rjust(len(str(n_lines))), '/', n_lines
+    
+    # continued lines
+    if line.startswith(' '):
+        # Need to figure out what sort of previous entry, so can append properly
+        # ***
+        if 'can_repeat' not in fields_properties[star_field_name]:
+            doc[current_field] += ' ' + line.strip()
+        else:
+            # ***
+            if 'block' not in fields_properties[star_field_name]:
+                doc[current_field][index] += ' ' + line.strip()
+            else:
+                doc[current_block][index][current_field] += ' ' + line.strip()
+        continue
 
-	for ii, line in enumerate(data_in):
-		if ii % 10000 == 0:
-			print str(ii).rjust(len(str(n_lines))), '/', n_lines
-		
-		if line.startswith(' '):
-			# Need to figure out what sort of previous entry, so can append properly
-			if key not in sales_repeats:
-				doc[current_field] += ' ' + line.strip()
-			else:
-				if key not in sales_blocks:
-					doc[current_field][index] += ' ' + line.strip()
-				else:
-					doc[current_block][index][current_field] += ' ' + line.strip()
-			continue
-	
-		key = line[:17].strip()
-		value = line[17:].strip()
-	
-		# blank lines
-		if not key:
-			continue
-	
-		if key == '--RECORD NUMBER--':
-			if current_field is not None:		# don't save on first line of the file
-				db.sales.save(doc)
-			doc = {}
-			current_field = 'record_number'	# or could do None...
-			doc['record_number'] = value
-			doc['data_file'] = data_file
-		else:
-			# Check first to see if field name is one we know about
-			if key not in sales_fields:
-				sys.exit('Problem with key ' + key + ' line ' + str(ii))	
-			else:
-				field = sales_fields[key]
+    star_field_name = line[:17].strip()
+    value = line[17:].strip()
 
-				# This is where real tests start for constructing document
-				# TODO: This is where type changes or additional parsing needs to happen on value...
-			
-				# Repeats False
-				if key not in sales_repeats:
-					# key == record number taken care of above, so only F covered here
-					current_field = field
-					doc[field] = value
-			
-				# Repeats True
-				else:
-				
-					# Field == current_field False
-					if field != current_field:
-						current_field = field
-						index = 0
-					
-					# Field == current_field True
-					else:
-						index += 1
-				
-					# Blocks False -- List of items
-					if key not in sales_blocks:
-					
-						current_block = None
+    # blank lines
+    if not star_field_name:
+        continue
 
-						if field not in doc:
-							doc[field] = []
-						
-						doc[field].append(value)
-				
-					# Blocks True -- List of dicts / objects
-					else:
-						block = sales_blocks[key]
-					
-						# Block == current_block False
-						if block != current_block:
-							current_block = block
+    if star_field_name == '--RECORD NUMBER--':
+        
+        # Save the previous record to DB when hit the next
+        if current_field is not None:       # don't save on first line of the file
+            db.descriptions.save(doc)
+        doc = {}
+        current_field = 'record_number'     # or could do None...
+        doc['record_number'] = value
+        doc['data_file'] = descriptions_file
+    
+    else:
+        
+        # Check first to see if field name is one we know about
+        # ***
+        if star_field_name not in fields_properties:
+            sys.exit('Problem with key ' + key + ' line ' + str(ii))    
+        else:
+            # ***
+            db_field = fields_properties[star_field_name]['db_field_name']
 
-						if block not in doc:
-							doc[block] = []
-					
-						if len(doc[block]) < index + 1:
-							doc[block].append({})
-					
-						doc[block][index][field] = value
-					
-			
+            # This is where real tests start for constructing document
+            # TODO: This is where type changes or additional parsing needs to happen on value...
+        
+            # Repeats False
+            # ***
+            if 'can_repeat' not in fields_properties[star_field_name]:
+                # key == record number taken care of above, so only F covered here
+                current_field = db_field
+                doc[db_field] = value
+        
+            # Repeats True
+            else:
+            
+                # Field == current_field False
+                if db_field != current_field:
+                    current_field = db_field
+                    index = 0
+                
+                # Field == current_field True
+                else:
+                    index += 1
+            
+                # Blocks False -- List of items
+                # ***
+                if 'block' not in fields_properties[star_field_name]:
+                
+                    current_block = None
+
+                    if db_field not in doc:
+                        doc[db_field] = []
+                    
+                    doc[db_field].append(value)
+            
+                # Blocks True -- List of dicts / objects
+                else:
+                    # ***
+                    block = fields_properties[star_field_name]['block']
+                
+                    # Block == current_block False
+                    if block != current_block:
+                        current_block = block
+
+                    if block not in doc:
+                        doc[block] = []
+                
+                    if len(doc[block]) < index + 1:
+                        doc[block].append({})
+                
+                    doc[block][index][db_field] = value
+                
+        
