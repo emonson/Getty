eebb508ac14e3373a31dcdf20326c1aea3f5cf24
Eric E Monson <emonson@cs.duke.edu>
2014-12-08 11:25:07 -0500

    progress in price parsing

--- price_parsing.py
+++ price_parsing.py
@@ -8,6 +8,7 @@ import sys
 import codecs
 import collections
 from pymongo import Connection
+from pymongo.errors import ConnectionFailure
 
 # Get data directly from MongoDB
 # Make a connection to Mongo.
@@ -75,31 +76,14 @@ tag_patterns = [(r'[0-9]+(?:(?:\.|:)[0-9]+){0,2}', 'NUM'),
 # Defining tokenizing patterns directly from tag patterns so only defined once
 token_patterns = '|'.join([x for x,y in tag_patterns]).strip('|')
 tokenizer = nltk.tokenize.RegexpTokenizer(token_patterns, flags=re.UNICODE)
-
 regexp_tagger = nltk.RegexpTagger(tag_patterns)
 
-
-# Chunker
-# TODO: Still need OR variants...
-price_grammar = '''
-    AMT:    {<NUM><CUR><NUM>}
-                {<NUM><CUR>}
-'''
-ch_amt = nltk.RegexpParser(price_grammar)
-
-# Note: the chunker seems to be NOT greedy, so it'll quit as soon as it
-#   finds a match (and won't continue as long as it can)...
-# TODO: still need num, num & num variant...
-lots_grammar = '''
-    INC:    {<LOTS><NOS>(<NUM><RNG><NUM>)}
-                {<LOTS><NOS>(<AND>?<NUM>(<LOTMOD>|<LOTRNG>)?)+}
-'''
-ch_lots = nltk.RegexpParser(lots_grammar)
-
 re_subfield = re.compile(r'(\|\w) *')
-
 tag_sets_counter = collections.Counter()
 
+# Switch this to False if only want to explore price fields and not really update DB
+UPDATE = True
+DEBUG = False
 VERBOSE = False
 
 # File method
@@ -107,12 +91,26 @@ VERBOSE = False
 #   cols = line.split(',')
 #   price = cols[2].strip(' "\n\r')
 # Direct from MongoDB method
-for ii,entry in enumerate(db.contents.find({'price':{'$exists':True},'country_authority':'France'},{'price':True,'lot_number':True}).limit(20)):
+query = {'price':{'$exists':True},'country_authority':'France'}
+n_records = db.contents.find(query).count()
+
+for ii,entry in enumerate(db.contents.find(query,{'price':True,'lot_number':True,'price_decimal':True})):
     
+    if ii % 10000 == 0:
+        print str(ii).rjust(len(str(n_records))), '/', n_records
+
     price = entry['price']
-    print u'———————————'
-    print entry
+    # print u'———————————'
+    # print entry
     
+    # Prepare update document
+    ID = {'_id':entry['_id']}
+    up = {}
+    up['$set'] = {}
+    upset = up['$set']
+    up['$unset'] = {}
+    upunset = {}
+            
     # split on |c type subsection divider
     price_subfields = re_subfield.split(price)
     subfield_flag = None
@@ -144,84 +142,211 @@ for ii,entry in enumerate(db.contents.find({'price':{'$exists':True},'country_au
             tag_set = ' '.join([b for (_,b) in tagged])
             tag_sets_counter[tag_set] += 1
             
-            # DEBUG
-            if tag_set.find('LOTMOD LOTMOD LOTMOD') >= 0:
-                print price_field, '--', entry['lot_number']
-                # print '_'.join(tokens)
+            if DEBUG:
+                if tag_set.find('NUM ECU NUM') >= 0:
+                    print price_field, '--', entry['lot_number']
+                    # print '_'.join(tokens)
+            
+            if UPDATE:
 
-#              
-#             # Chunking 
-#             if subfield_flag == None:
-#                 tree = ch_amt.parse(tagged)
-#                 print tree
-#                 for subtree in tree.subtrees():
-#                     if subtree.label() == 'AMT':
-#                         print 'AMT', subtree.leaves()
-#         
-#             elif subfield_flag == 'notes':
-#                 tree = ch_lots.parse(tagged)
-#                 n_incs = 0
-#                 for subtree in tree.subtrees():
-#                     if subtree.label() == 'INC':
-#                         # Note: problem printing subtree if one of the entries is a unicode non-ascii...
-#                         print 'INC', subtree.leaves()
-#                         n_incs += 1
-#                 if n_incs == 0:
-#                     print '*** NOTES NOT CAUGHT **', len(price)
-#         
+                # -------------------
+                # Actual updates
+
+                if tag_set == 'NUM LIVRES':
+                    upset['price_decimal'] = float(tagged[0][0])
+                    upset['currency'] = 'livres'
+                    upunset['unparsed_price'] = ''
             
-            # -------------------
-            # Actual updates
-            ID = {'_id':entry['_id']}
-            up = {}
-            up['$set'] = {}
-            upset = up['$set']
+                elif tag_set == 'NUM LIVRES NUM':
+                    upset['price_decimal'] = float(tagged[0][0]) + float(tagged[2][0])/20.0
+                    upset['currency'] = 'livres'
+                    upunset['unparsed_price'] = ''
             
-            if tag_set == 'NUM LIVRES':
-                upset['price_decimal'] = float(tagged[0][0])
-                upset['currency'] = 'livres'
-                db.contents.update(ID, up, upsert=False, multi=False)
+                elif tag_set == 'NUM FRANCS':
+                    upset['price_decimal'] = float(tagged[0][0])
+                    upset['currency'] = 'francs'
+                    upunset['unparsed_price'] = ''
+
+                elif tag_set == 'POUR LES LOTS NOS NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+                
+                # NOTE: Using 100 sub-francs
+                elif tag_set == 'NUM FRANCS NUM':
+                    upset['price_decimal'] = float(tagged[0][0]) + float(tagged[2][0])/100.0
+                    upset['currency'] = 'francs'
+                    upunset['unparsed_price'] = ''
+                    
+                elif tag_set == 'NUM LIVRES NUM SOLS':
+                    upset['price_decimal'] = float(tagged[0][0]) + float(tagged[2][0])/20.0
+                    upset['currency'] = 'livres'
+                    upunset['unparsed_price'] = ''
+
+                elif tag_set == 'POUR LES LOTS NOS NUM AND NUM':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NOS NUM LOTMOD AND NUM LOTMOD':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NUM LOTMOD AND LOTMOD':
+                    upset['unparsed_lots'] = True
+                    
+                elif tag_set == 'NUM ASSIGNATS':
+                    upset['price_decimal'] = float(tagged[0][0])
+                    upset['currency'] = 'assignats'
+                    upunset['unparsed_price'] = ''
+                
+                # NOTE: Flag for two prices is existance of price_decimal_or field
+                elif tag_set == 'NUM OR NUM LIVRES':
+                    upset['price_decimal'] = float(tagged[0][0])
+                    upset['price_decimal_or'] = float(tagged[2][0])
+                    upset['currency'] = 'livres'
+                    upunset['unparsed_price'] = ''
+
+                elif tag_set == 'POUR LES LOTS NOS NUM LOTMOD AND LOTMOD':
+                    upset['unparsed_lots'] = True
+                
+                elif tag_set == 'LIVRES':
+                    upset['currency'] = 'livres'
+                    upunset['unparsed_price'] = ''
+                
+                elif tag_set == 'NUM':
+                    upset['price_decimal'] = float(tagged[0][0])
+                    upset['currency'] = 'unknown'
+                    upunset['unparsed_price'] = ''
+                    
+                elif tag_set == 'POUR LES LOTS NOS NUM LOTRNG AND NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NOS NUM RNG NUM':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NOS NUM LOTRNG AND NUM':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NOS NUM AND NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NUM AND NUM':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NUM LOTMOD AND NUM LOTMOD':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS X LOTRNG':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NOS NUM AND NUM LOTMOD':
+                    upset['unparsed_lots'] = True
+
+                elif tag_set == 'FRANCS':
+                    upset['currency'] = 'francs'
+                    upunset['unparsed_price'] = ''
+
+                # NOTE: Flag for two prices is existance of price_decimal_or field
+                elif tag_set == 'NUM OR NUM FRANCS':
+                    upset['price_decimal'] = float(tagged[0][0])
+                    upset['price_decimal_or'] = float(tagged[2][0])
+                    upset['currency'] = 'francs'
+                    upunset['unparsed_price'] = ''
+
+                elif tag_set == 'POUR LES LOTS NOS NUM NUM AND NUM':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NUM LOTRNG AND NUM':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NUM LOTRNG AND NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NOS LOTMOD NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'ILLEGIBLE':
+                    pass
+                elif tag_set == 'POUR LES LOTS NOS NUM LOTMOD LOTMOD AND LOTMOD':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS LOTMOD NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+                    
+                # NOTE: Using 20 sub-assignats
+                elif tag_set == 'NUM ASSIGNATS NUM':
+                    upset['price_decimal'] = float(tagged[0][0]) + float(tagged[2][0])/20.0
+                    upset['currency'] = 'assignats'
+                    upunset['unparsed_price'] = ''
+                    
+                # NOTE: Flag for two prices is existance of price_decimal_or field
+                elif tag_set == 'NUM FRANCS OR NUM FRANCS':
+                    upset['price_decimal'] = float(tagged[0][0])
+                    upset['price_decimal_or'] = float(tagged[3][0])
+                    upset['currency'] = 'francs'
+                    upunset['unparsed_price'] = ''
+
+                elif tag_set == 'POUR LES LOTS NOS NUM LOTMOD LOTRNG':
+                    upset['unparsed_lots'] = True
+                
+                elif tag_set == 'NUM FRANCS QU':
+                    upset['unparsed_price'] = True
+                
+                elif tag_set == 'POUR LES NOS NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+                
+                elif tag_set == 'NUM LIVRES QU':
+                    upset['unparsed_price'] = True
+                
+                elif tag_set == 'POUR LES LOTS NUM AND NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+
+                # NOTE: Flag for two prices is existance of price_decimal_or field
+                elif tag_set == 'NUM FRANCS NUM OR NUM FRANCS':
+                    upset['price_decimal'] = float(tagged[0][0]) + float(tagged[2][0])/100.0
+                    upset['price_decimal_or'] = float(tagged[4][0])
+                    upset['currency'] = 'francs'
+                    upunset['unparsed_price'] = ''
+
+                elif tag_set == 'POUR LES LOTS NOS LOTMOD NUM AND LOTMOD NUM':
+                    upset['unparsed_lots'] = True
+
+                # NOTE: Using 20 sub-ecus
+                elif tag_set == 'NUM ECU NUM':
+                    upset['price_decimal'] = float(tagged[0][0]) + float(tagged[2][0])/20.0
+                    upset['currency'] = 'ecus'
+                    upunset['unparsed_price'] = ''
+
+                elif tag_set == 'POUR LES LOTS NOS NUM LOTMOD LOTMOD AND NUM LOTMOD LOTMOD':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NOS X LOTMOD AND X LOTMOD':
+                    upset['unparsed_lots'] = True
+                
+                elif tag_set == 'LIVRES NUM':
+                    upset['price_decimal'] = float(tagged[1][0])/20.0
+                    upset['currency'] = 'livres'
+                    upunset['unparsed_price'] = ''
+                
+                elif tag_set == 'AVEC LES LOTS NUM':
+                    upset['unparsed_lots'] = True
+                
+                elif tag_set == 'NUM LIVRES NUM QU':
+                    upset['unparsed_price'] = True
+                elif tag_set == 'NUM QU FRANCS':
+                    upset['unparsed_price'] = True
+                
+                elif tag_set == 'POUR LES LOTS NOS NUM LOTRNG AND NUM LOTRNG AND NUM LOTRNG':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES NOS NUM LOTMOD AND NUM LOTMOD':
+                    upset['unparsed_lots'] = True
+                elif tag_set == 'POUR LES LOTS NOS NUM NUM RNG NUM':
+                    upset['unparsed_lots'] = True
+                
+                elif tag_set == 'NUM CUR LOTMOD':
+                    upset['unparsed_price'] = True
+                elif tag_set == 'QU':
+                    upset['unparsed_price'] = True
+
+                elif tag_set == 'NUM ECU':
+                    upset['price_decimal'] = float(tagged[0][0])
+                    upset['currency'] = 'ecus'
+                    upunset['unparsed_price'] = ''
+
+                elif tag_set.startswith('NUM'):
+                    upset['unparsed_price'] = True
+                elif tag_set.startswith('POUR') or tag_set.startswith('AVEC'):
+                    upset['unparsed_lots'] = True           
             
-            elif tag_set == 'NUM LIVRES NUM':
-                upset['price_decimal'] = tagged[0][0] + tagged[2][0]/20.0
-                upset['currency'] = 'livres'
-                db.contents.update(ID, up, upsert=False, multi=False)
             
-            elif tag_set == 'NUM FRANCS':
-                upset['price_decimal'] = float(tagged[0][0])
-                upset['currency'] = 'francs'
-                db.contents.update(ID, up, upsert=False, multi=False)
-            
-            elif tag_set == 'POUR LES LOTS NOS NUM LOTRNG':
-                pass
-            elif tag_set == 'NUM FRANCS NUM':
-                pass
-            elif tag_set == 'NUM LIVRES NUM SOLS':
-                pass
-            elif tag_set == 'POUR LES LOTS NOS NUM AND NUM':
-                pass
-            elif tag_set == 'POUR LES LOTS NOS NUM LOTMOD AND NUM LOTMOD':
-                pass
-            elif tag_set == 'POUR LES LOTS NUM LOTRNG':
-                pass
-            elif tag_set == 'POUR LES LOTS NUM LOTMOD AND LOTMOD':
-                pass
-            elif tag_set == 'NUM ASSIGNATS':
-                pass
-            elif tag_set == 'NUM OR NUM LIVRES':
-                pass
-            elif tag_set == 'POUR LES LOTS NOS NUM LOTMOD AND LOTMOD':
-                pass
-            elif tag_set == 'LIVRES':
-                pass
-            elif tag_set == 'NUM':
-                pass
-            
-            
-            
-            
-            
-            
+        # -------------------
+        # Do actual update of document in DB with new fields
+        if UPDATE:
+            db.contents.update(ID, up, upsert=False, multi=False)
+
         if VERBOSE: print
         # Only hits this after processing a line, not a tag
         subfield_flag = None
@@ -229,8 +354,8 @@ for ii,entry in enumerate(db.contents.find({'price':{'$exists':True},'country_au
 print
 print 'number of tag sets', len(tag_sets_counter)
 
-# for tagset in sorted(tag_sets_counter):
-#     print str(tag_sets_counter[tagset]).rjust(5), tagset
-for ii,tagset in enumerate(sorted(tag_sets_counter.items(), key=lambda x: x[1], reverse=True)):
-    print str(ii).rjust(3), str(tagset[1]).rjust(5), tagset[0]
-    # print tagset[1]
+for tagset in sorted(tag_sets_counter):
+    print str(tag_sets_counter[tagset]).rjust(5), tagset
+# for ii,tagset in enumerate(sorted(tag_sets_counter.items(), key=lambda x: x[1], reverse=True)):
+#     print str(ii).rjust(3), str(tagset[1]).rjust(5), tagset[0]
+#     # print tagset[1]
